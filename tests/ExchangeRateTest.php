e62626fba0059715e6ae513f9c76b16074c49467
herloct <herloct@gmail.com>
2014-11-11 22:36:49 +0700

    Create basic test for ExchageRate class

---
+++ tests/ExchangeRateTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Kuartet\BI\ExchangeRate;
+
+use \Carbon\Carbon;
+use \PHPUnit_Framework_TestCase;
+
+class ExchangeRateTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $code = 'IDR';
+        $name = 'Indonesian Rupiah';
+        $sell = 10000;
+        $buy = 11000;
+        $middle = 10500;
+        $updatedAt = Carbon::now();
+
+        $exchangeRate = new ExchangeRate($code, $name, $sell, $buy, $middle, $updatedAt);
+        $this->assertInstanceOf('Kuartet\BI\ExchangeRate\ExchangeRateInterface', $exchangeRate);
+        $this->assertEquals($code, $exchangeRate->getCode());
+        $this->assertEquals($name, $exchangeRate->getName());
+        $this->assertEquals($sell, $exchangeRate->getSell());
+        $this->assertEquals($but, $exchangeRate->getBuy());
+        $this->assertEquals($middle, $exchangeRate->getMiddle());
+        $this->assertEquals($updatedAt, $exchangeRate->getUpdatedAt());
+    }
+}
