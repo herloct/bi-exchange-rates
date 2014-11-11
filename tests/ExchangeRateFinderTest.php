8b8333f80e729dfc8a52c374202428ee2d6dcb8c
herloct <herloct@gmail.com>
2014-11-12 00:55:25 +0700

    Check some rates

--- tests/ExchangeRateFinderTest.php
+++ tests/ExchangeRateFinderTest.php
@@ -26,5 +26,23 @@ class ExchangeRateFinderTest extends PHPUnit_Framework_TestCase
         $this->assertInternalType('array', $exchangeRates);
         $this->assertContainsOnlyInstancesOf(ExchangeRateInterface::class, $exchangeRates);
         $this->assertCount(22, $exchangeRates);
+
+        // Check AUD
+        $rate = $exchangeRates[0];
+        $this->assertEquals('AUD', $rate->getCode());
+        $this->assertEquals('AUSTRALIAN DOLLAR', $rate->getName());
+        $this->assertEquals(10571.32, $rate->getSell());
+        $this->assertEquals(10460.97, $rate->getBuy());
+        $this->assertEquals((10571.32 + 10460.97) / 2, $rate->getMiddle());
+        $this->assertEquals(\Carbon\Carbon::parse('11 November 2014'), $rate->getUpdatedAt());
+
+        // Check JPY which have value 100
+        $rate = $exchangeRates[9];
+        $this->assertEquals('JPY', $rate->getCode());
+        $this->assertEquals('JAPANESE YEN', $rate->getName());
+        $this->assertEquals(10655.51/100, $rate->getSell());
+        $this->assertEquals(10546.41/100, $rate->getBuy());
+        $this->assertEquals((10655.51 + 10546.41) / 200, $rate->getMiddle());
+        $this->assertEquals(\Carbon\Carbon::parse('11 November 2014'), $rate->getUpdatedAt());
     }
 }
