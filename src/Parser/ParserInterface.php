0a1501712f5ca76b63aa8716c3a5c72e8cb766d7
herloct <herloct@gmail.com>
2014-11-12 19:42:59 +0700

    Create parser interface

---
+++ src/Parser/ParserInterface.php
@@ -0,0 +1,20 @@
+<?php
+namespace Kuartet\BI\Parser;
+
+use \Kuartet\BI\Domain\RateInterface;
+
+/**
+ * HTML parser interface
+ *
+ * @author herloct <herloct@gmail.com>
+ */
+interface ParserInterface
+{
+    /**
+     * Parse HTML to Rates
+     *
+     * @param  string          $html HTML to parse
+     * @return RateInterface[] Array of rates
+     */
+    public function parse($html);
+}
