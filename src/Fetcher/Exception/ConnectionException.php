9561a155401a644cb53e9e4e0f90df7d48440bde
herloct <herloct@gmail.com>
2014-11-12 18:44:23 +0700

    Create connection exception

---
+++ src/Fetcher/Exception/ConnectionException.php
@@ -0,0 +1,26 @@
+<?php
+namespace Kuartet\BI\Fetcher\Exception;
+
+use \Exception;
+use \RuntimeException;
+
+/**
+ * Connection exception
+ *
+ * For handling connection error, http code 5xx, and http code 4xx
+ *
+ * @author herloct
+ */
+class ConnectionException extends RuntimeException
+{
+    /**
+     * Constructor
+     *
+     * @param string    $message  Exception message
+     * @param Exception $previous Previous exception
+     */
+    public function __construct($message, Exception $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
+}
